8b002b5590e4147d5fc145ef9578cb1d4bb841fc
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2023-12-12 16:04:44 -0500

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git subtree push --prefix api heroku-backend main
-git subtree push --prefix client heroku-frontend main
+git subtree push --prefix api heroku-server main
+git subtree push --prefix client heroku-client main
 
 echo "Deployment finished!"
