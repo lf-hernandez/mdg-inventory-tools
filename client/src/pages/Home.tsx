0a558750e7d6c12337ad6d54a34c1f3d49ff3198
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2023-12-27 22:48:16 -0500

    Add sub-view menu to home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,48 @@
+import { useState } from "react";
 import { AddItemForm } from "../components/AddItemForm";
 import { ItemList } from "../components/ItemList";
 import { SearchForm } from "../components/SearchForm";
 
 const Home = () => {
+  const [view, setView] = useState("search");
+
   return (
     <div className="mx-auto max-w-7xl p-4">
-      <SearchForm />
-      <br />
-      <AddItemForm />
-      <br />
-      <ItemList />
+      <div className="flex justify-center space-x-4 mb-6">
+        <button
+          className={`px-4 py-2 rounded-lg font-semibold transition-colors ${
+            view === "search"
+              ? "bg-blue-500 text-white"
+              : "bg-gray-100 hover:bg-blue-500 hover:text-white"
+          }`}
+          onClick={() => setView("search")}
+        >
+          Search Inventory
+        </button>
+        <button
+          className={`px-4 py-2 rounded-lg font-semibold transition-colors ${
+            view === "add"
+              ? "bg-blue-500 text-white"
+              : "bg-gray-100 hover:bg-blue-500 hover:text-white"
+          }`}
+          onClick={() => setView("add")}
+        >
+          Add Inventory
+        </button>
+        <button
+          className={`px-4 py-2 rounded-lg font-semibold transition-colors ${
+            view === "list"
+              ? "bg-blue-500 text-white"
+              : "bg-gray-100 hover:bg-blue-500 hover:text-white"
+          }`}
+          onClick={() => setView("list")}
+        >
+          View Inventory
+        </button>
+      </div>
+      {view === "search" && <SearchForm />}
+      {view === "add" && <AddItemForm />}
+      {view === "list" && <ItemList />}
     </div>
   );
 };
