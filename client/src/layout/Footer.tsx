0cc2a5afd757db00525666d0688ecf80313643d2
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-04-07 23:30:31 -0400

    Update copyright

--- client/src/layout/Footer.tsx
+++ client/src/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="bg-gray-100 py-4 text-center w-full">
-      <small>&copy; 2023 MDG Inventory Manager</small>
+      <small>&copy; 2024 MDG Inventory Manager</small>
     </footer>
   );
 };
