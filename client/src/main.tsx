7ffeb753f541953ba151e55315697d8190848d9f
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-04-08 21:45:57 -0400

    Fix env var scheme as per vite convention
    "To prevent accidentally leaking env variables to the client, only
    variables prefixed with VITE_ are exposed to your Vite-processed code."
    
    https://vitejs.dev/guide/env-and-mode.html#env-files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import * as amplitude from "@amplitude/analytics-browser";
 import App from "./App";
 import "./index.css";
 
-amplitude.init(import.meta.env.AMPLITUDE_API_KEY);
+amplitude.init(import.meta.env.VITE_AMPLITUDE_API_KEY);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
