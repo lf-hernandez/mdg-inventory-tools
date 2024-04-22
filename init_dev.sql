0dce1b9e9308bacc5ffe276b9622aaf45229279f
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-04-21 23:45:48 -0400

    Ensure crypto extension exists

--- init_dev.sql
+++ init_dev.sql
@@ -1,4 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE TABLE inventory (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL,
